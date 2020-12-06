ffd192bb7d7fcaf079587962e234b4c49a581e96
Alice Boxhall <aboxhall@chromium.org>
2020-12-06 16:28:29 +1100

    day six part 2

--- six/six.js
+++ six/six.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
 const data = fs.readFileSync(0, 'utf-8');
 
-const groups = data.split('\n\n').map((group) => new Set(Array.from(group.replaceAll('\n', ''))));
-let count = groups.reduce((acc, group) =>  acc + group.size, 0);
+let temp_questions = [];
+const charCodeA = "a".charCodeAt(0);
+for (let i = 0; i < 26; i++) {
+    const charCode = charCodeA + i;
+    temp_questions.push(String.fromCharCode(charCode));
+}
+const questions = temp_questions;
+    
+const groups = data.split('\n\n').map((group) => {
+    let people = group.split('\n');
+    let intersection= questions.slice();
+    for (const person of people) {
+	if (person === "")
+	    continue;
+	intersection = intersection.filter(q => person.includes(q));
+    }
+    return intersection;
+});
+
+let count = groups.reduce((acc, group) =>  acc + group.length, 0);
 console.log({count});
 
