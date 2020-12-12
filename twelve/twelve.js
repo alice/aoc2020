001b9d84cb385c837c3081b44845b7cc47d9ad4b
Alice Boxhall <aboxhall@chromium.org>
2020-12-13 07:51:14 +1100

    day 12 part 2

--- twelve/twelve.js
+++ twelve/twelve.js
@@ -14,33 +14,43 @@ const HEADING_LETTERS = Array.from(Object.keys(HEADINGS));
 
 const instr_re = /(?<action>[NESWLRF])(?<value>\d+)/;
 let position = [0, 0];
-let heading = 'E';
+let waypoint = [10, 1];
 
 for (instruction of instructions) {
     const {action, value} = instruction.match(instr_re).groups;
     switch (action) {
     case 'F': {
-	for ([axis, m] of HEADINGS[heading].entries())
+	for ([axis, m] of waypoint.entries())
 	    position[axis] += m * value;
 	break;
     }
     case 'R': {
-	const h = ((value / 90) + HEADING_LETTERS.indexOf(heading)) % HEADING_LETTERS.length;
-	heading = HEADING_LETTERS[h];
+	let quarter_turns = value / 90;
+	for (let i = 0; i < quarter_turns; i++) {
+	    let temp_waypoint = [];
+	    temp_waypoint[0] = waypoint[1];
+	    temp_waypoint[1] = waypoint[0] * -1;
+	    waypoint = temp_waypoint;
+	}
 	break;
     }
     case 'L': {
-	const h = ((-value / 90) + HEADING_LETTERS.indexOf(heading) + HEADING_LETTERS.length)
-	    % HEADING_LETTERS.length;
-	heading = HEADING_LETTERS[h];
+	let quarter_turns = value / 90;
+	for (let i = 0; i < quarter_turns; i++) {
+	    let temp_waypoint = [];
+	    temp_waypoint[0] = waypoint[1] * -1;
+	    temp_waypoint[1] = waypoint[0];
+	    waypoint = temp_waypoint;
+	}
 	break;
     }
     default: {
 	for ([axis, m] of HEADINGS[action].entries()) 
-	    position[axis] += m * value;
+	    waypoint[axis] += m * value;
 	break;
     }
     }
+    console.log({action, value, waypoint, position});
 }
 
 let manhattan_distance = Math.abs(position[0]) + Math.abs(position[1]);
