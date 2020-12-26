44a2387a7e9452244becbf6e2a83850914590029
Alice Boxhall <aboxhall@chromium.org>
2020-12-26 22:01:52 +1100

    day 16 part 2

--- sixteen/sixteen.js
+++ sixteen/sixteen.js
@@ -29,12 +29,57 @@ let scanning_error_rate = 0;
 let good_tickets = [];
 for (let ticket of nearby_tickets) {
     let error_rate = errorRate(ticket);
-    scanning_error_rate += errorRate;
+    scanning_error_rate += error_rate;
     if (error_rate === 0)
 	good_tickets.push(ticket);
 }
 
-console.log(scanning_error_rate);
+console.log({scanning_error_rate});
+
+let positions_valid_for = [];
+for (let i = 0; i < good_tickets[0].length; i++) {
+    positions_valid_for[i] = new Set();
+    for (let name of named_ranges.keys())
+	positions_valid_for[i].add(name);
+}
+
+for (let ticket of good_tickets) {
+    for (let p = 0; p < ticket.length; p++) {
+	for (let [name, ranges] of named_ranges) {
+	    if (!isValidForRanges(ticket[p], ranges))
+		positions_valid_for[p].delete(name);
+	}    
+    }
+}
+
+let position_names = [];
+while (true) {
+    let found_name = false;
+    for (let p = 0; p < good_tickets[0].length; p++) {
+	if (positions_valid_for[p].size !== 1)
+	    continue;
+	let name = positions_valid_for[p].values().next().value; // seriously
+	found_name = true;
+	position_names[p] = name;
+	for (let set of positions_valid_for)
+	    set.delete(name);
+    }
+    if (!found_name)
+	break;
+}
+
+let my_ticket = data_segments[1].split(',').map((s) => Number.parseInt(s));
+let departure_total = 1;
+for (let [p, name] of position_names.entries()) {
+    if (!name)
+	continue;
+    if (!name.startsWith('departure'))
+	continue;
+    let value = my_ticket[p];
+    departure_total *= value;
+}
+
+console.log({departure_total});
 
 function errorRate(ticket) {
     let error_rate = 0;
