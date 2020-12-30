93683f9c7bba746f3b254eeb41c31b1da44686a4
Alice Boxhall <aboxhall@chromium.org>
2020-12-30 16:18:05 +1100

    day 19 part 2

--- nineteen/nineteen.js
+++ nineteen/nineteen.js
@@ -12,6 +12,14 @@ class Rule {
 	this._number = number;
 	this._letter = letter;
 	this._sub_rules = sub_rules || [];
+	this.recursive = false;
+	for (let rule of this._sub_rules) {
+	    if (!rule.includes(this._number))
+		continue;
+	    
+	    this.recursive = true;
+	    break;
+	}
 
 	this.dependencies = new Set();
 	for (let sub_rule of this._sub_rules) {
@@ -41,11 +49,44 @@ class Rule {
 	}
     }
 
-    generateStrings() {
-	if (this._strings) {
-	    return this._strings;
+    consumeMatch(potential_match) {
+	if (!this.recursive) {
+	    let potential_partials = [];
+	    for (let string of this._strings) {
+		if (potential_match.length < string.length)
+		    continue;
+		if (potential_match.startsWith(string))
+		    potential_partials.push(potential_match.slice(string.length));
+	    }
+	    return potential_partials;
 	}
 
+	// recursive case
+	let potential_partials = []
+	for (let sub_rule of this._sub_rules) {
+	    let rules = sub_rule.map((n) => all_rules[n]);
+	    let subrule_potential_partials = [potential_match];
+	    while (rules.length) {
+		let rule = rules.shift();
+		let new_potential_partials = [];
+		for (let potential_partial of subrule_potential_partials) {
+		    new_potential_partials = new_potential_partials
+			.concat(rule.consumeMatch(potential_partial));
+		}
+		subrule_potential_partials = new_potential_partials;
+	    }
+	    potential_partials = potential_partials.concat(subrule_potential_partials);
+	}
+	return potential_partials;
+    }
+
+    generateStrings() {
+	if (this.recursive)
+	    return null;
+	
+	if (this._strings)
+	    return this._strings;
+	
 	if (this._letter) {
 	    this._strings = [this._letter];
 	    return this._strings;
@@ -55,6 +96,11 @@ class Rule {
 	for (const sub_rule of this._sub_rules) {
 	    let strings = [''];
 	    for (const num of sub_rule) {
+		let rule = all_rules[num];
+		if (rule.recursive) {
+		    this.recursive = true;
+		    return null;
+		}
 		let sub_strings = all_rules[num].generateStrings();
 		strings = strings.map((str) => sub_strings.map((sub_str) => str + sub_str))
 		    .flat();
@@ -69,7 +115,7 @@ const data = fs.readFileSync(0, 'utf-8');
 let [rules_lines, messages_lines] = data.split('\n\n');
 
 for (let rule_line of rules_lines.split('\n')) {
-    const {idx, rule} = rule_line.match(RULE_RE).groups;
+    let {idx, rule} = rule_line.match(RULE_RE).groups;
     const i = Number.parseInt(idx);
     const letter_match = rule.match(LETTER_RE);
     if (letter_match) {
@@ -77,6 +123,10 @@ for (let rule_line of rules_lines.split('\n')) {
 	continue;
     }
     assert(rule.match(SUB_RULES_RE), rule);
+    if (i === 8)
+	rule = '42 | 42 8';
+    if (i === 11)
+	rule = '42 31 | 42 11 31';
     const rule_parts = rule.split(' | ');
     let sub_rules = [];
     for (let part of rule_parts)
@@ -85,6 +135,8 @@ for (let rule_line of rules_lines.split('\n')) {
 }
 
 for (let rule of all_rules) {
+    if (!rule)
+	continue;
     rule.notifyDependencies();
 }
 
@@ -102,5 +154,7 @@ while (to_be_generated.length) {
 
 let all_strings = new Set(all_rules[0].generateStrings());
 let messages = messages_lines.split('\n');
-let matching_messages = messages.filter((message) => all_strings.has(message));
+let matching_messages = messages.filter((message) =>
+					all_rules[0].consumeMatch(message).some((partial) =>
+										partial === ''));
 console.log(`${matching_messages.length} matching messages`);
